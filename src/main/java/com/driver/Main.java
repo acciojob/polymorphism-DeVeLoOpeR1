5e4acef1952bebce416d2e58e119fe98b7410dbf
saurabh yadav <saurabh.sy2001>
2023-01-13 18:08:11 +0530

    fourth commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,6 +2,17 @@ package com.driver;
 
 public class Main {
 
+    static class Product{
+        int product(int x, int y) {
+            return x+y;
+        }
+        int product(int x, int y, int z) {
+            return x+y+z;
+        }
+        double product(double x, double y) {
+            return x+y;
+        }
+    }
 public static void main(String[] args){
     Product p = new Product();
     System.out.println(p.product(4,5));
@@ -11,14 +22,4 @@ public static void main(String[] args){
 
 }
 }
-class Product{
-    int product(int x, int y) {
-        return x+y;
-    }
-    int product(int x, int y, int z) {
-        return x+y+z;
-    }
-    double product(double x, double y) {
-        return x+y;
-    }
-}
+

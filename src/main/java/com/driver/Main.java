30fcb10733f7184d8c90500a3de51c99624e6a12
saurabh yadav <saurabh.sy2001>
2023-01-13 17:40:11 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class Main {
+public static void main(String[] args){
+    Product p = new Product();
+    System.out.println(p.product(4,5));
+    System.out.println(p.product(4,5,7));
+    System.out.println(p.product(4.34,5.6));
 
+
+}
+}
+class Product{
+    public int product(int x, int y) {
+        return x+y;
+    }
+    public int product(int x, int y, int z) {
+        return x+y+z;
+    }
+    public double product(double x, double y) {
+        return x+y;
+    }
 }
